a0b1ace6f3ddc75d672d6ac640d9decda9804583
toadie-odie <sabakiyang@yahoo.com>
2022-12-20 12:01:22 -0500

    Initial commit

---
+++ src/main/java/net/warrentode/todecoins/TodeCoins.java
@@ -0,0 +1,39 @@
+package net.warrentode.todecoins;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(TodeCoins.MOD_ID)
+public class TodeCoins {
+    public static final String MOD_ID = "todecoins";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public TodeCoins() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
