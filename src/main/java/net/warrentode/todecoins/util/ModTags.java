ce5b034844d55c3abfccbbc64f50d70702dbc3bb
toadie-odie <sabakiyang@yahoo.com>
2022-12-29 19:20:01 -0500

    Got the recipe types working correctly, along with the durability of the coin stamp functioning within the coin press, removed the coins and banknotes from the crafting table recipe book, and added tags

--- src/main/java/net/warrentode/todecoins/util/ModTags.java
+++ src/main/java/net/warrentode/todecoins/util/ModTags.java
@@ -14,6 +14,13 @@ public class ModTags {
     public static final TagKey<Item> CURRENCY = modItemTag("currency");
     public static final TagKey<Item> GEMS = modItemTag("gems");
     public static final TagKey<Item> TEXTILES = modItemTag("textiles");
+    public static final TagKey<Item> COPPER_NUGGET = modItemTag("nuggets/copper");
+    public static final TagKey<Item> COPPER_INGOT = modItemTag("ingots/copper");
+    public static final TagKey<Item> IRON_NUGGET = modItemTag("nuggets/iron");
+    public static final TagKey<Item> IRON_INGOT = modItemTag("ingots/iron");
+    public static final TagKey<Item> GOLD_NUGGET = modItemTag("nuggets/gold");
+    public static final TagKey<Item> GOLD_INGOT = modItemTag("ingots/gold");
+    public static final TagKey<Item> NETHERITE_INGOT = modItemTag("ingots/netherite");
 
     private static TagKey<Block> modBlockTag(String path) {
         return TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(TodeCoins.MOD_ID, path));
