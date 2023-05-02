e63ed9728c9aa222f5a09799fe7938bdd188e71b
toadie-odie <sabakiyang@yahoo.com>
2023-05-01 20:19:47 -0400

    chest loot
    Signed-off-by: toadie-odie <sabakiyang@yahoo.com>

--- src/main/java/net/warrentode/todecoins/datagen/loot/ModChestLootTables.java
+++ src/main/java/net/warrentode/todecoins/datagen/loot/ModChestLootTables.java
@@ -27,46 +27,46 @@ public class ModChestLootTables extends ChestLoot {
     public static final LootItemCondition.Builder IN_BASALT_DELTAS = LocationCheck.checkLocation(LocationPredicate.Builder.location().setBiome(Biomes.BASALT_DELTAS));
     public static final LootItemCondition.Builder IN_SOUL_SAND_VALLEY = LocationCheck.checkLocation(LocationPredicate.Builder.location().setBiome(Biomes.SOUL_SAND_VALLEY));
 
-    private static final ResourceLocation RECIPEBOOKTEST_CHEST_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_chest");
-    private static final ResourceLocation RECIPEBOOKTEST_COPPER_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_copper_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_IRON_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_iron_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_GOLD_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_gold_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_LUCKY_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_lucky_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_NETHERITE_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_netherite_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_NETHER_GOLD_COIN_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_nether_gold_coin_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_EMERALD_QUARTER_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_emerald_quarter_bank_note_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_EMERALD_HALF_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_emerald_half_bank_note_loot");
-    private static final ResourceLocation RECIPEBOOKTEST_EMERALD_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/recipebooktest/recipebooktest_emerald_bank_note_loot");
+    private static final ResourceLocation TODECOINS_CHEST_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_chest");
+    private static final ResourceLocation TODECOINS_COPPER_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_copper_coin_loot");
+    private static final ResourceLocation TODECOINS_IRON_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_iron_coin_loot");
+    private static final ResourceLocation TODECOINS_GOLD_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_gold_coin_loot");
+    private static final ResourceLocation TODECOINS_LUCKY_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_lucky_coin_loot");
+    private static final ResourceLocation TODECOINS_NETHERITE_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_netherite_coin_loot");
+    private static final ResourceLocation TODECOINS_NETHER_GOLD_COIN_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_nether_gold_coin_loot");
+    private static final ResourceLocation TODECOINS_EMERALD_QUARTER_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_emerald_quarter_bank_note_loot");
+    private static final ResourceLocation TODECOINS_EMERALD_HALF_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_emerald_half_bank_note_loot");
+    private static final ResourceLocation TODECOINS_EMERALD_BANK_NOTE_LOOT = new ResourceLocation(MODID, "chests/todecoins/todecoins_emerald_bank_note_loot");
 
     @Override
     public void accept(BiConsumer<ResourceLocation, LootTable.Builder> consumer) {
-        consumer.accept(RECIPEBOOKTEST_CHEST_LOOT,
+        consumer.accept(TODECOINS_CHEST_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(ConstantValue.exactly(1.0F))
                                           .setBonusRolls(ConstantValue.exactly(1.0F))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_COPPER_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_COPPER_COIN_LOOT)
                                                        .setWeight(7).setQuality(0))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_IRON_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_IRON_COIN_LOOT)
                                                        .setWeight(6).setQuality(1))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_GOLD_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_GOLD_COIN_LOOT)
                                                        .setWeight(5).setQuality(2))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_NETHER_GOLD_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_NETHER_GOLD_COIN_LOOT)
                                                        .when(IN_NETHER_WASTES.or(IN_CRIMSON_FOREST).or(IN_WARPED_FOREST).or(IN_BASALT_DELTAS).or(IN_SOUL_SAND_VALLEY))
                                                        .setWeight(5).setQuality(2))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_EMERALD_QUARTER_BANK_NOTE_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_EMERALD_QUARTER_BANK_NOTE_LOOT)
                                                        .setWeight(4).setQuality(3))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_NETHERITE_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_NETHERITE_COIN_LOOT)
                                                        .setWeight(4).setQuality(4))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_EMERALD_HALF_BANK_NOTE_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_EMERALD_HALF_BANK_NOTE_LOOT)
                                                        .setWeight(3).setQuality(4))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_EMERALD_BANK_NOTE_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_EMERALD_BANK_NOTE_LOOT)
                                                        .setWeight(2).setQuality(5))
-                                          .add(LootTableReference.lootTableReference(RECIPEBOOKTEST_LUCKY_COIN_LOOT)
+                                          .add(LootTableReference.lootTableReference(TODECOINS_LUCKY_COIN_LOOT)
                                                        .setWeight(1).setQuality(6))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_COPPER_COIN_LOOT,
+        consumer.accept(TODECOINS_COPPER_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 7.0F))
@@ -76,7 +76,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.COPPER_COIN_BAG.get()))
                 )
         );
-        consumer.accept(RECIPEBOOKTEST_IRON_COIN_LOOT,
+        consumer.accept(TODECOINS_IRON_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 6.0F))
@@ -86,7 +86,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.IRON_COIN_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_GOLD_COIN_LOOT,
+        consumer.accept(TODECOINS_GOLD_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 5.0F))
@@ -96,7 +96,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.GOLD_COIN_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_NETHER_GOLD_COIN_LOOT,
+        consumer.accept(TODECOINS_NETHER_GOLD_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 5.0F))
@@ -106,7 +106,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.NETHER_GOLD_COIN_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_EMERALD_QUARTER_BANK_NOTE_LOOT,
+        consumer.accept(TODECOINS_EMERALD_QUARTER_BANK_NOTE_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 4.0F))
@@ -116,7 +116,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.EMERALD_QUARTER_BANK_NOTE_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_NETHERITE_COIN_LOOT,
+        consumer.accept(TODECOINS_NETHERITE_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 3.0F))
@@ -126,7 +126,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.NETHER_GOLD_COIN_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_EMERALD_HALF_BANK_NOTE_LOOT,
+        consumer.accept(TODECOINS_EMERALD_HALF_BANK_NOTE_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 3.0F))
@@ -136,7 +136,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.EMERALD_HALF_BANK_NOTE_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_EMERALD_BANK_NOTE_LOOT,
+        consumer.accept(TODECOINS_EMERALD_BANK_NOTE_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(UniformGenerator.between(1.0F, 2.0F))
@@ -146,7 +146,7 @@ public class ModChestLootTables extends ChestLoot {
                                           .add(LootItem.lootTableItem(ModBlocks.EMERALD_BANK_NOTE_BAG.get()))
                         )
         );
-        consumer.accept(RECIPEBOOKTEST_LUCKY_COIN_LOOT,
+        consumer.accept(TODECOINS_LUCKY_COIN_LOOT,
                 LootTable.lootTable()
                         .withPool(LootPool.lootPool()
                                           .setRolls(ConstantValue.exactly(1.0F))
