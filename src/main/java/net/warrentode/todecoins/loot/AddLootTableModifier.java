8c189788e6e833d5ef05c6398e933e5e84bf508c
toadie-odie <3997423+toadie-odie@users.noreply.github.com>
2023-05-06 05:33:33 -0400

    chest loot modifiers added and working
    Signed-off-by: toadie-odie <3997423+toadie-odie@users.noreply.github.com>

--- src/main/java/net/warrentode/todecoins/loot/AddLootTableModifier.java
+++ src/main/java/net/warrentode/todecoins/loot/AddLootTableModifier.java
@@ -4,8 +4,6 @@ import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import java.util.function.Supplier;
-import javax.annotation.Nonnull;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -14,18 +12,24 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 
+import javax.annotation.Nonnull;
+import java.util.function.Supplier;
+
 /**
- Credits to Commoble for this implementation! */
+ * Credits to Commoble for this implementation!
+ */
 public class AddLootTableModifier extends LootModifier {
     public static final Supplier<Codec<AddLootTableModifier>> CODEC = Suppliers.memoize(
-			() -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ResourceLocation.CODEC.fieldOf("lootTable").forGetter((m) -> m.lootTable)).apply(inst, AddLootTableModifier::new)));
+            () -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ResourceLocation.CODEC.fieldOf("lootTable").forGetter((m) -> m.lootTable)).apply(inst, AddLootTableModifier::new)));
     private final ResourceLocation lootTable;
+
     protected AddLootTableModifier(LootItemCondition[] conditionsIn, ResourceLocation lootTable) {
         super(conditionsIn);
         this.lootTable = lootTable;
     }
+
     @SuppressWarnings("deprecation")
-	@Nonnull
+    @Nonnull
     @Override
     protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
 		LootTable extraTable = context.getLootTable(this.lootTable);
