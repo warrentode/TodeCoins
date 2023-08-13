a7c3d82e7036b99c04a8229251f4b79256cc060c
Warren Tode <3997423+warrentode@users.noreply.github.com>
2023-08-13 18:08:55 -0400

    revert:
    since attempts to setup the mod for dependency use via github didn't work, I changed it all back to how it was before
    
    Signed-off-by: Warren Tode <3997423+warrentode@users.noreply.github.com>

--- src/main/java/com/github/warrentode/todecoins/util/tags/ModTags.java
+++ src/main/java/com/github/warrentode/todecoins/util/tags/ModTags.java
@@ -121,6 +121,7 @@ public class ModTags {
         public static final TagKey<Item> WITHER_COIN_SET = modItemTag("collectible_coins/entity_coins/wither_coin_set");
         public static final TagKey<Item> WITHER_SKELETON_COIN_SET = modItemTag("collectible_coins/entity_coins/wither_skeleton_coin_set");
         public static final TagKey<Item> WOLF_COIN_SET = modItemTag("collectible_coins/entity_coins/wolf_coin_set");
+        public static final TagKey<Item> ZOGLIN_COIN_SET = modItemTag("collectible_coins/entity_coins/zoglin_coin_set");
         public static final TagKey<Item> ZOMBIE_HORSE_COIN_SET = modItemTag("collectible_coins/entity_coins/zombie_horse_coin_set");
         public static final TagKey<Item> ZOMBIFIED_PIGLIN_COIN_SET = modItemTag("collectible_coins/entity_coins/zombified_piglin_coin_set");
 
