8790eb6167449c29eb7cc5ceba8f3aa890116092
Warren Tode <3997423+warrentode@users.noreply.github.com>
2023-09-22 20:56:00 -0400

    9/22/2023 - [patch] finally took care of the slightly wonky quickMoveStack method in the CoinPress, and added a script to get the deobf jar I need for dependency use via JitPack
    Signed-off-by: Warren Tode <3997423+warrentode@users.noreply.github.com>

--- src/main/java/com/github/warrentode/todecoins/gui/coinpressgui/CoinPressMenu.java
+++ src/main/java/com/github/warrentode/todecoins/gui/coinpressgui/CoinPressMenu.java
@@ -23,12 +23,27 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class CoinPressMenu extends RecipeBookMenu<RecipeWrapper> {
+    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
+    // must assign a slot number to each of the slots used by the GUI.
+    // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
+    // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
+    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
+    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
+    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
+    private static final int HOTBAR_SLOT_COUNT = 9;
+    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
+    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
+    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
+    private static final int VANILLA_LAST_SLOT_INDEX = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = 0;
+    private static final int RESULT_SLOT_INDEX = 2;
+    private static final int TE_INVENTORY_LAST_SLOT_INDEX = RESULT_SLOT_INDEX + 1;  // must be the number of slots you have!
+    private static final int VANILLA_FIRST_SLOT_INDEX = TE_INVENTORY_LAST_SLOT_INDEX + 1;
     public final CoinPressBlockEntity coinPress;
+    public final ItemStackHandler inventory;
     private final Level level;
     private final ContainerData coinpressData;
     private final ContainerLevelAccess canInteractWithCallable;
-    public final ItemStackHandler inventory;
-
     public CoinPressMenu(final int windowId, final Inventory playerInventory, final FriendlyByteBuf extraData) {
         this(windowId, playerInventory, getBlockEntity(playerInventory, extraData), new SimpleContainerData(2));
     }
@@ -52,9 +67,7 @@ public class CoinPressMenu extends RecipeBookMenu<RecipeWrapper> {
 
         this.addDataSlots(coinpressData);
     }
-    public ItemStackHandler getInventory() {
-        return inventory;
-    }
+
     private static CoinPressBlockEntity getBlockEntity(final Inventory playerInventory, final FriendlyByteBuf data) {
         Objects.requireNonNull(playerInventory, "playerInventory cannot be null");
         Objects.requireNonNull(data, "data cannot be null");
@@ -64,6 +77,11 @@ public class CoinPressMenu extends RecipeBookMenu<RecipeWrapper> {
         }
         throw new IllegalStateException("Tile entity is not correct! " + tileAtPos);
     }
+
+    public ItemStackHandler getInventory() {
+        return inventory;
+    }
+
     public boolean isCrafting() {
         return coinpressData.get(0) > 0;
     }
@@ -76,51 +94,37 @@ public class CoinPressMenu extends RecipeBookMenu<RecipeWrapper> {
         return maxProgress != 0 && progress != 0 ? progress * progressArrowSize / maxProgress : 0;
     }
 
-    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
-    // must assign a slot number to each of the slots used by the GUI.
-    // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
-    // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
-    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
-    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
-    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
-    private static final int HOTBAR_SLOT_COUNT = 9;
-    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
-    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
-    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
-    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
-    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
-
-    // THIS YOU HAVE TO DEFINE!
-    private static final int TE_INVENTORY_SLOT_COUNT = 3;  // must be the number of slots you have!
-
     @Override
     public @NotNull ItemStack quickMoveStack(@NotNull Player playerIn, int index) {
         Slot sourceSlot = slots.get(index);
-        if (!sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
+        if (!sourceSlot.hasItem()) {
+            return ItemStack.EMPTY;  //EMPTY_ITEM
+        }
         ItemStack sourceStack = sourceSlot.getItem();
         ItemStack copyOfSourceStack = sourceStack.copy();
 
         // Check if the slot clicked is one of the vanilla container slots
-        if (index < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
+        if (index > RESULT_SLOT_INDEX) {
             // This is a vanilla container slot so merge the stack into the tile inventory
-            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX
-                    + TE_INVENTORY_SLOT_COUNT, false)) {
+            if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, RESULT_SLOT_INDEX, false)) {
                 return ItemStack.EMPTY;  // EMPTY_ITEM
             }
-        } else if (index < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
+        }
+        else if (index < VANILLA_FIRST_SLOT_INDEX) {
             // This is a TE slot so merge the stack into the players inventory
-            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_LAST_SLOT_INDEX, false)) {
                 return ItemStack.EMPTY;
             }
-        } else {
+        }
+        else {
             System.out.println("Invalid slotIndex:" + index);
             return ItemStack.EMPTY;
         }
         // If stack size == 0 (the entire stack was moved) set slot contents to null
         if (sourceStack.getCount() == 0) {
             sourceSlot.set(ItemStack.EMPTY);
-        } else {
+        }
+        else {
             sourceSlot.setChanged();
         }
         sourceSlot.onTake(playerIn, sourceStack);
