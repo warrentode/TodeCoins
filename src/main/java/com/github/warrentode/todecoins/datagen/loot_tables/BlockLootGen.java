317c1f948bae91454b0dfe746b1d500011a4683f
Warren Tode <3997423+warrentode@users.noreply.github.com>
2025-09-09 05:44:29 -0400

    added missing block loot tables

--- src/main/java/com/github/warrentode/todecoins/datagen/loot_tables/BlockLootGen.java
+++ src/main/java/com/github/warrentode/todecoins/datagen/loot_tables/BlockLootGen.java
@@ -3,53 +3,45 @@ package com.github.warrentode.todecoins.datagen.loot_tables;
 import com.github.warrentode.todecoins.block.TCBlocks;
 import com.github.warrentode.todecoins.item.TCItems;
 import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.github.warrentode.todecoins.TodeCoins.MODID;
 
 public class BlockLootGen extends BlockLootSubProvider {
-    private final Set<Block> blockTables = new HashSet<>();
-
     public BlockLootGen() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
     @Override
     protected void generate() {
-        this.dropSelf(TCBlocks.COINPRESSBLOCK.get());
-        this.dropSelf(TCBlocks.POT_OF_GOLD.get());
-
-        this.dropSelf(TCBlocks.ENDONIAN_BLOCK.get());
-
-        this.dropSelf(TCBlocks.COPPER_COIN_BAG.get());
-        this.dropSelf(TCBlocks.IRON_COIN_BAG.get());
-        this.dropSelf(TCBlocks.GOLD_COIN_BAG.get());
-        this.dropSelf(TCBlocks.NETHER_GOLD_COIN_BAG.get());
-        this.dropSelf(TCBlocks.NETHERITE_COIN_BAG.get());
-        this.dropSelf(TCBlocks.ENDONIAN_COIN_BAG.get());
-        this.dropSelf(TCBlocks.LUCKY_COIN_BAG.get());
-
-        this.dropSelf(TCBlocks.EMERALD_QUARTER_BANK_NOTE_BAG.get());
-        this.dropSelf(TCBlocks.EMERALD_HALF_BANK_NOTE_BAG.get());
-        this.dropSelf(TCBlocks.EMERALD_BANK_NOTE_BAG.get());
-
-
         this.add(TCBlocks.ENDONIAN_ORE.get(),
                 (block) -> this.createOreDrop(block, TCItems.ENDONIAN_CRYSTAL.get()));
-    }
 
-    @Override
-    protected void add(@NotNull Block block, @NotNull LootTable.Builder builder) {
-        this.blockTables.add(block);
-        this.map.put(block.getLootTable(), builder);
+        List<Block> blockList = TCBlocks.BLOCKS.getEntries().stream()
+                .map(RegistryObject::get).filter(block -> {
+                    ResourceLocation location = ForgeRegistries.BLOCKS.getKey(block);
+                    return location != null && !location.getPath().contains("ore");
+                })
+                .toList();
+
+        blockList.forEach(this::dropSelf);
     }
 
     @Override
     protected @NotNull Iterable<Block> getKnownBlocks() {
-        return blockTables;
+        return ForgeRegistries.BLOCKS.getValues().stream()
+                .filter(block -> MODID.equals(Objects.requireNonNull(
+                                ForgeRegistries.BLOCKS.getKey(block))
+                        .getNamespace())).collect(Collectors.toSet());
     }
 }
